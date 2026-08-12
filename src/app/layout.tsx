66e5c1b9c68dfd26b99e0d248522781e9b69134f
Dominik Stempel <dominik.stempel7@gmail.com>
2026-08-12 22:35:26 +0200

    feat: add goat counter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; // 1. Import Script from Next.js
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Script
+          strategy="afterInteractive"
+          data-goatcounter="https://domiko.goatcounter.com/count"
+          src="//gc.zgo.at/count.js"
+        />
+      </body>
     </html>
   );
 }
